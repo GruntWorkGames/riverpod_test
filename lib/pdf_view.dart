73b298a18de70b908d9b419014a77f63d4525f2b
Kris Akers <kris.akers101@gmail.com>
2023-04-17 21:15:44 -0500

    pdf viewloading with async riverpod state

--- lib/pdf_view.dart
+++ lib/pdf_view.dart
@@ -1,88 +1,33 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 
-class PDFProvider extends StateNotifier<PdfView> {
-  PDFProvider()
-      : super(const PdfView(
-          path: "",
-        ));
-
-  void setPdfState(PdfView newState) {
-    updateShouldNotify(state, newState);
-    state = newState;
-  }
-}
-
-final pdfProvider = StateNotifierProvider<PDFProvider, PdfView>((ref) {
-  return PDFProvider();
+var pdfProvider = FutureProvider<PDFView>((ref) async {
+  const sampleUrl =
+      'https://mini-iac.org/downloads/sequences/2023-knowns/download-file?path=2023+Unlimited.pdf';
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/imac.pdf');
+  final response = await http.get(Uri.parse(sampleUrl));
+  await file.writeAsBytes(response.bodyBytes);
+  return PDFView(filePath: file.path);
 });
 
 // ignore: must_be_immutable
 class PDFScreen extends ConsumerWidget {
-  final sampleUrl =
-      'https://mini-iac.org/downloads/sequences/2023-knowns/download-file?path=2023+Unlimited.pdf';
-  String pdfFlePath = "";
+  var shouldDisplay = false;
   final progressIndicator = const CircularProgressIndicator();
   var loaderShowing = false;
-  var fileExists = false;
-
-  PDFScreen({Key? key}) : super(key: key);
-
-  Future<void> downloadAndSavePdf(BuildContext context, WidgetRef ref) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/sample.pdf');
-    fileExists = await file.exists();
-    var message = "Downloading PDF...";
-    if (fileExists) {
-      message = "Loading PDF...";
-    }
-
-    // ignore: use_build_context_synchronously
-    showDialog(
-        barrierDismissible: false,
-        useSafeArea: true,
-        context: context,
-        builder: (context) {
-          return Material(
-              type: MaterialType.transparency,
-              child: Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  progressIndicator,
-                  Container(
-                      margin: const EdgeInsets.only(top: 15),
-                      child: Builder(
-                        builder: (context) {
-                          return Text(message,
-                              style: const TextStyle(
-                                  color: Colors.white, fontSize: 24));
-                        },
-                      ))
-                ],
-              )));
-        });
-
-    loaderShowing = true;
-    pdfFlePath = file.path;
-
-    // if (fileExists) {
-    //   ref.read(pdfProvider.notifier).state = (PdfView(path: file.path));
-    //   return;
-    // }
-
-    final response = await http.get(Uri.parse(sampleUrl));
-    await file.writeAsBytes(response.bodyBytes);
-    ref.read(pdfProvider.notifier).setPdfState(PdfView(path: pdfFlePath));
-  }
+  var fileLoaded = false;
+  PDFScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final pdfView = ref.watch(pdfProvider);
+    AsyncValue<PDFView> pdfView = ref.watch(pdfProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text("PDF View"), actions: [
@@ -93,20 +38,49 @@ class PDFScreen extends ConsumerWidget {
             icon: const Icon(Icons.refresh)),
       ]),
       body: Center(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Builder(
-            builder: (context) {
-              if (loaderShowing) {
-                loaderShowing = false;
-                Navigator.of(context).pop();
-              }
-              return Expanded(child: Builder(builder: (BuildContext context) {
-                return PdfView(path: pdfView.path);
-              }));
-            },
-          )
-        ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+                child: pdfView.when(
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (err, stack) => Text('Error: $err'),
+              data: (pdf) {
+                return pdf;
+              },
+            )),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> downloadAndSavePdf(BuildContext context, WidgetRef ref) async {
+    // ignore: use_build_context_synchronously
+    // showDialog(
+    //     barrierDismissible: false,
+    //     useSafeArea: true,
+    //     context: context,
+    //     builder: (context) {
+    //       return Material(
+    //           type: MaterialType.transparency,
+    //           child: Center(
+    //               child: Column(
+    //             mainAxisAlignment: MainAxisAlignment.center,
+    //             children: [
+    //               // progressIndicator,
+    //               // Container(
+    //               //     margin: const EdgeInsets.only(top: 15),
+    //               //     child: Builder(
+    //               //       builder: (context) {
+    //               //         return const Text("loading PDF",
+    //               //             style:
+    //               //                 TextStyle(color: Colors.white, fontSize: 24));
+    //               //       },
+    //               //     ))
+    //             ],
+    //           )));
+    //     });
   }
 }
