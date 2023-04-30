abe36630e3b8801c1b47ebe07d288ee0cda41637
Kris Akers <>
2023-04-29 21:09:49 -0500

    re enabled pdf

--- lib/pdf_view.dart
+++ lib/pdf_view.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
 
 const key = 'customCacheKey';
 CacheManager pdfCacheManager = CacheManager(
   Config(
     key,
     stalePeriod: const Duration(minutes: 1),
-    maxNrOfCacheObjects: 200,
+    maxNrOfCacheObjects: 2,
   ),
 );
 
-// var pdfProvider = FutureProvider<PDFView>((ref) async {
-//   const sampleUrl =
-//       'https://mini-iac.org/downloads/sequences/2023-knowns/download-file?path=2023+Unlimited.pdf';
-//   var file = await pdfCacheManager.getFileFromCache("pdf");
-//
-//   if (file == null) {
-//     print("downloading");
-//     await pdfCacheManager.downloadFile(sampleUrl, key: "pdf");
-//     file = await pdfCacheManager.getFileFromCache("pdf");
-//   } else {
-//     print("loading from cache");
-//   }
-//
-//   return PDFView(filePath: file?.file.path);
-// });
+var pdfProvider = FutureProvider<PdfView>((ref) async {
+  const sampleUrl =
+      'https://mini-iac.org/downloads/sequences/2023-knowns/download-file?path=2023+Unlimited.pdf';
+  var file = await pdfCacheManager.getFileFromCache(key);
+
+  if (file == null) {
+    print("downloading");
+    await pdfCacheManager.downloadFile(sampleUrl, key: key);
+    file = await pdfCacheManager.getFileFromCache(key);
+  } else {
+    print("loading from cache");
+  }
+
+  return PdfView(path: file?.file.path ?? "");
+});
 
 // ignore: must_be_immutable
 class PDFScreen extends ConsumerWidget {
@@ -37,7 +38,7 @@ class PDFScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // AsyncValue<PDFView> pdfView = ref.watch(pdfProvider);
+    AsyncValue<PdfView> pdfView = ref.watch(pdfProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text("PDF View")),
@@ -46,15 +47,14 @@ class PDFScreen extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container()
-            // Expanded(
-            //     child: pdfView.when(
-            //   loading: () => const Center(child: CircularProgressIndicator()),
-            //   error: (err, stack) => Text('Error: $err'),
-            //   data: (pdf) {
-            //     return pdf;
-            //   },
-            // )),
+            Expanded(
+                child: pdfView.when(
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (err, stack) => Text('Error: $err'),
+              data: (pdf) {
+                return pdf;
+              },
+            )),
           ],
         ),
       ),
